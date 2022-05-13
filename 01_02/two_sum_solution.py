85d13ff76094ae529ee1c75c914078ecf29903f4
unknown <prandrews@hotmail.co.uk>
2022-05-13 09:27:20 +0100

    Video code.

---
+++ 01_02/two_sum_solution.py
@@ -0,0 +1,17 @@
+# 2-Sum Interview Problem Solution
+
+# Brute force approach
+def two_sum_problem(arr, target):
+    length = len(arr)
+    for i in range(length - 1):
+        for j in range(1, length):
+            if arr[i] + arr[j] == target:
+                return i, j
+    return None
+
+
+assert two_sum_problem([1, 2, 3], 4) == (0, 2)
+assert two_sum_problem([1234, 5678, 9012], 14690) == (1, 2)
+assert two_sum_problem([2, 2, 3], 4) in [(0, 1), (1, 0)]
+assert two_sum_problem([2, 2], 4) in [(0, 1), (1, 0)]
+assert two_sum_problem([8, 7, 2, 5, 3, 1], 10) in [(0, 2), (2, 0), (1, 4), (4, 1)]
